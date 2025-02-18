70fd2b1f25877fabf7dfea1095f2729c82b67220
ismael <joseismaelvargas@users.noreply.github.com>
2025-02-17 21:51:44 -0300

    Se cambio la imagen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ onAddTodo:({title}:TodoTitle)=>void
 const Header:React.FC<Props> = ({onAddTodo}) => {
   return (
     <header>
-       <h1>todo<img src='https://www.typescriptlang.org/' style={{width:'60px',height:'auto'}}></img></h1> 
+       <h1>todo<img src='https://tse3.mm.bing.net/th?id=OIP.vTTaLQ3lHcr8G6cNGmti9gHaEK&pid=Api&P=0&h=180' style={{width:'60px',height:'auto'}}></img></h1> 
     <CreateTodo saveTodo={onAddTodo}></CreateTodo>
     </header>
   )
